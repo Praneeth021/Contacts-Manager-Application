d60620f361f7bb6587ce91637d8d5d2a2847f0d8
Praneeth <kadampallypraneeth987@gmail.com>
2025-01-16 14:35:04 -0500

    Added Material UI Components to contactList

--- contacts_manager_frontend/src/components/contactList.js
+++ contacts_manager_frontend/src/components/contactList.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { List, ListItem, ListItemText, IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 const ContactList = ({ contacts, onDelete }) => (
-    <ul>
-        {contacts.map((contact) => (
-            <li key={contact.id}>
-                {contact.name} - {contact.email}{' '}
-                <button onClick={() => onDelete(contact.id)}>Delete</button>
-            </li>
-        ))}
-    </ul>
+    <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+            {contacts.map((contact) => (
+                <ListItem
+                    key={contact.id}
+                    secondaryAction={
+                        <IconButton edge="end" aria-label="delete" onClick={() => onDelete(contact.id)}>
+                            <DeleteIcon color="error" />
+                        </IconButton>
+                    }
+                >
+                    <ListItemText primary={contact.name} secondary={contact.email} />
+                </ListItem>
+            ))}
+    </List>
 );
 
 export default ContactList;
